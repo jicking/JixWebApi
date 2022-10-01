7d099567bdc217068d9e8976ae5776288b946b23
jicking <7297029+jicking@users.noreply.github.com>
2022-10-02 03:21:24 +0800

    set NLOG for logging

--- JixWebApi/Program.cs
+++ JixWebApi/Program.cs
@@ -1,34 +1,51 @@
 using JixWebApi.Core.Services;
+using NLog;
+using NLog.Web;
 
 namespace JixWebApi;
 public class Program {
 	public static void Main(string[] args) {
-		var builder = WebApplication.CreateBuilder(args);
 
-		// Add services to the container.
-		builder.Services.AddRazorPages();
+		var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+		logger.Debug("Init main JixWebAPI =================");
 
-		// Custom Services
-		builder.Services.AddScoped<IProjectService, ProjectService>();
+		try {
+			var builder = WebApplication.CreateBuilder(args);
 
-		var app = builder.Build();
+			// Add services to the container.
+			builder.Services.AddRazorPages();
 
-		// Configure the HTTP request pipeline.
-		if (!app.Environment.IsDevelopment()) {
-			app.UseExceptionHandler("/Error");
-			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-			app.UseHsts();
+			// Custom Services
+			builder.Services.AddScoped<IProjectService, ProjectService>();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment()) {
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapRazorPages();
+
+			app.Run();
 		}
-
-		app.UseHttpsRedirection();
-		app.UseStaticFiles();
-
-		app.UseRouting();
-
-		app.UseAuthorization();
-
-		app.MapRazorPages();
-
-		app.Run();
+		catch (Exception exception) {
+			// NLog: catch setup errors
+			logger.Error(exception, "Stopped JixWebAPI because of exception =================");
+			throw;
+		}
+		finally {
+			// Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+			NLog.LogManager.Shutdown();
+		}
 	}
 }
