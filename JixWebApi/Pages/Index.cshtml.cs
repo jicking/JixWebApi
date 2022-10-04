8dd273335a1694bf5703a8809aede268828a495e
jicking <7297029+jicking@users.noreply.github.com>
2022-10-04 16:08:50 +0800

    Show App name and Env

--- JixWebApi/Pages/Index.cshtml.cs
+++ JixWebApi/Pages/Index.cshtml.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace JixWebApi.Pages {
 	public class IndexModel : PageModel {
 		private readonly ILogger<IndexModel> _logger;
 
-		public IndexModel(ILogger<IndexModel> logger) {
+		public readonly IWebHostEnvironment WebHostEnv;
+
+		public IndexModel(
+			ILogger<IndexModel> logger,
+			IWebHostEnvironment webHostEnv
+			) {
 			_logger = logger;
+			WebHostEnv = webHostEnv;
 		}
 
 		public void OnGet() {
