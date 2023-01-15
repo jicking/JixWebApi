1025bef2f69df27ef93a8405d8cf8ba198af954a
jicking bebiro <7297029+jicking@users.noreply.github.com>
2023-01-15 12:28:38 +0800

    Update comment

--- content/JixWebApp.Core/Data/JixWebAppDbContextFactory.cs
+++ content/JixWebApp.Core/Data/JixWebAppDbContextFactory.cs
@@ -3,8 +3,12 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace JixWebApp.Data;
 
+// This is required when scaffolding Razor Page/Controller
+// because EF dbcontext is on a separate project library.
+// Expecting  that you are using EF inmemory db,
+// if not then update the options builder (eg: sqlite, sql).
 // Disable this code when running EF migrations
-// Might only need this at initial prototyping stage anyways
+// will need this at initial prototyping stage anyways.
 public class JixWebAppDbContextFactory : IDesignTimeDbContextFactory<JixWebAppDbContext> {
 	JixWebAppDbContext IDesignTimeDbContextFactory<JixWebAppDbContext>.CreateDbContext(string[] args) {
 		var optionsBuilder = new DbContextOptionsBuilder<JixWebAppDbContext>();
